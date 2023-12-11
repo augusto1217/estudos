3b9140bdbec3335980f76ae6bfd5eb8b30f7c46b
Augusto Mariano <augusto1217@gmail.com>
2023-12-11 20:11:21 -0300

    Ajuste para configurar ambiente

--- angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/services/DBService.java
+++ angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/services/DBService.java
@@ -30,16 +30,20 @@ public class DBService {
     public void instanciaDB() {
         
         Tecnico tec1 = new Tecnico(null, "Jane Soares Maia", "99915143187", "janes@email.com", "123");
-		tec1.setPerfil(Perfil.ADMIN);		
+		tec1.setPerfil(Perfil.ADMIN);
+
+		Tecnico tec2 = new Tecnico(null, "Deborah Soares Maia", "89715143187", "debinha@email.com", "1234");
+		tec1.setPerfil(Perfil.CLIENTE);		
 
 		Cliente cli1 = new Cliente(null, "Linus Torvalds", "69158592119", "linus@linux.org", "12345");
 
 		Chamado ch1 = new Chamado(null, Status.ANDAMENTO, tec1, cli1, "O Cliente deseja instalar mais memória em seu notebook", Prioridade.MEDIA);
 		Chamado ch2 = new Chamado(null, Status.ABERTO, tec1, cli1, "O Cliente deseja instalar uma impressora", Prioridade.BAIXA);
+		Chamado ch3 = new Chamado(null, Status.ABERTO, tec2, cli1, "O Cliente deseja instalar uma impressora", Prioridade.BAIXA);
 
-		tecnicoRepository.saveAll(List.of(tec1));
+		tecnicoRepository.saveAll(List.of(tec1, tec2));
 		clienteRepository.saveAll(List.of(cli1));
-		chamadoRepository.saveAll(List.of(ch1,ch2));
+		chamadoRepository.saveAll(List.of(ch1,ch2,ch3));
 
     }
     
